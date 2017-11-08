13a61e607d09ceaa5c2c574a2c26d9c108e41b3e
mustafa3max <abofatmaalslman@gmail.com>
2017-11-08 16:55:34 +0300

    Create myJs.js

---
+++ myJs.js
@@ -0,0 +1,53 @@
+/* border-bottom-hover1 */
+function overMuose1() {
+	document.getElementById("border-bottom-hover1").style = "border-bottom: 5px solid #C96507";
+}
+
+
+function outMuose1() {
+	document.getElementById("border-bottom-hover1").style = "border-bottom: 5px solid# fff";
+}
+
+/* border-bottom-hover2 */
+function overMuose2() {
+	document.getElementById("border-bottom-hover2").style = "border-bottom: 5px solid #C96507";
+}
+
+
+function outMuose2() {
+	document.getElementById("border-bottom-hover2").style = "border-bottom: 5px solid# fff";
+}
+
+/* border-bottom-hover3 */
+function overMuose3() {
+	document.getElementById("border-bottom-hover3").style = "border-bottom: 5px solid #C96507";
+}
+
+
+function outMuose3() {
+	document.getElementById("border-bottom-hover3").style = "border-bottom: 5px solid# fff";
+}
+
+/* border-bottom-hover4 */
+function overMuose4() {
+	document.getElementById("border-bottom-hover4").style = "border-bottom: 5px solid #C96507";
+}
+
+
+function outMuose4() {
+	document.getElementById("border-bottom-hover4").style = "border-bottom: 5px solid# fff";
+}
+
+
+/* show call my */
+function showCallMy() {
+	document.getElementById("show-call-my").style = "display: block";
+	document.getElementById("heddin-show").style = "display: none";
+	document.getElementById("all-call").style = "transition: 3s; transform: translateY(-505px)";
+}
+
+function heddinCallMy() {
+	document.getElementById("show-call-my").style = "display: none; transition: 3s; transform: translateY(-505px)";
+	document.getElementById("heddin-show").style = "display: block";
+	document.getElementById("all-call").style = "";
+}
